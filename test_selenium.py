3d5ef7b0b00ea864fcc1eea5a0eedf3c307186c0
SamerDakh <31184455+SamerDakh@users.noreply.github.com>
2019-11-30 20:57:10 +0200

    Update test_selenium.py
    fix xpath

--- test_selenium.py
+++ test_selenium.py
@@ -11,13 +11,13 @@ class SearchText(unittest.TestCase):
 
     def test_ramadan_2019_pass(self):
         # validate title is 'مسلسلات رمضان 2019'
-        self.driver.find_element_by_xpath('/html/body/div[4]/div[3]/div[1]/a').click()
+        self.driver.find_element_by_xpath('/html/body/div[3]/div[1]/ul/li[8]/a').click()
         h1 = self.driver.find_element_by_xpath('//*[@id="left_content"]/div[2]/div[2]/h1')
         self.assertEqual( h1.text, 'مسلسلات رمضان 2019')
 
     def test_ramadan_2018_nigative(self):
         # validate title is not 'مسلسلات رمضان 2018'
-        self.driver.find_element_by_xpath('/html/body/div[4]/div[3]/div[1]/a').click()
+        self.driver.find_element_by_xpath('/html/body/div[3]/div[1]/ul/li[8]/a').click()
         h1 = self.driver.find_element_by_xpath('//*[@id="left_content"]/div[2]/div[2]/h1')
         self.assertNotEqual(h1.text, 'مسلسلات رمضان 2018')
 
